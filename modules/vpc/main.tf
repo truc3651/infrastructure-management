9672c0a06d776b9a0df961a029d2635ca21e81d8
truc2001 <thn2858@yum.com>
2026-02-09 12:40:43 +0700

    change git actions path

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -65,7 +65,7 @@ resource "aws_nat_gateway" "main" {
   count = var.enable_nat_gateway ? 1 : 0
 
   allocation_id = aws_eip.nat[0].id
-  subnet_id     = aws_subnet.public[0].id
+  subnet_id     = values(aws_subnet.public)[0].id
 
   tags = {
     Name = "${var.cluster_name}-nat"
